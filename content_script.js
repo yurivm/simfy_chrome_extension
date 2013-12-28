dd4387275b6f9b3d514e006f0f9ce0942281ebd7
yurivm <yuri.veremeyenko@gmail.com>
2013-12-28 22:02:31 +0100

    so finally I read the docs *all the way* to the end and quickly hacked something that works.

--- content_script.js
+++ content_script.js
@@ -1,1 +1,51 @@
 console.log("o hai, this is the content script");
+
+var playToggle = function() {
+  console.log("togglePlayer()");
+  if (Core.EI.player_is_playing) {
+    Core.EI.pausePlayback();
+  } else {
+    Core.EI.resumePlayback();
+  }
+};
+
+var playNext = function() {
+    console.log("playNext()");
+    Core.EI.playNext();
+};
+
+var playPrevious = function() {
+    console.log("playPrevious()");
+    Core.EI.playPrevious();
+};
+
+
+var injectScriptFunction = function(func) {
+  var script = document.createElement('script');
+  script.textContent = '(' + func + ')()';
+  (document.head||document.documentElement).appendChild(script);
+  script.parentNode.removeChild(script);
+}
+
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    var ack = request.command;
+    switch(request.command) {
+      case "PLAY_NEXT":
+        injectScriptFunction(playNext);
+        break;
+      case "PLAY_PREVIOUS":
+        injectScriptFunction(playPrevious);
+        break;
+      case "PLAY_TOGGLE":
+        injectScriptFunction(playToggle);
+        break;
+      default:
+        ack = "WHARRGARBL";
+    }
+    sendResponse({ack: ack});
+  });
--- popup.js
+++ popup.js
@@ -1,48 +1,41 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
-var QUERY = 'kittens';
-
 var simfyConnector = {
 
   playNext: function() {
     console.log("playNext clicked");
-     window.postMessage({ type: "SIMFY_EXTENSION_CMD", command: "PLAY_NEXT" }, "*");
+    chrome.tabs.query({url: "*://www.simfy.de/*"}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {command: "PLAY_NEXT"}, function(response) {
+        console.log("reply form the content script: " + response.ack);
+      });
+    });
+//     window.postMessage({ type: "SIMFY_EXTENSION_CMD", command: "PLAY_NEXT" }, "*");
   },
 
   playPrevious: function() {
     console.log("playPrevious clicked");
-    window.postMessage({ type: "SIMFY_EXTENSION_CMD", command: "PLAY_PREVIOUS" }, "*");
+    chrome.tabs.query({url: "*://www.simfy.de/*"}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {command: "PLAY_PREVIOUS"}, function(response) {
+        console.log("reply form the content script: " + response.ack);
+      });
+    });
+    //window.postMessage({ type: "SIMFY_EXTENSION_CMD", command: "PLAY_PREVIOUS" }, "*");
   },
 
-  pause: function() {
-    console.log("pause clicked");
-    window.postMessage({ type: "SIMFY_EXTENSION_CMD", command: "PLAY_PAUSE" }, "*");
-  },
-
-  play: function() {
+  playToggle: function() {
     console.log("play clicked");
-    window.postMessage({ type: "SIMFY_EXTENSION_CMD", command: "PLAY_RESUME" }, "*");
+    chrome.tabs.query({url: "*://www.simfy.de/*"}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {command: "PLAY_TOGGLE"}, function(response) {
+      console.log("reply form the content script: " + response.ack);
+      });
+    });
+    //window.postMessage({ type: "SIMFY_EXTENSION_CMD", command: "PLAY_RESUME" }, "*");
   }
 
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById("simfy_next").addEventListener("click", simfyConnector.playNext, false);
-  document.getElementById("simfy_prev").addEventListener("click", simfyConnector.playPrevious, false);
-  document.getElementById("simfy_play").addEventListener("click", simfyConnector.play, false);
+console.log("popup js started");
+$(document).ready(function() {
+  $("#simfy_next").click(simfyConnector.playNext);
+  $("#simfy_prev").click(simfyConnector.playPrevious);
+  $("#simfy_play").click(simfyConnector.playToggle);
 });
-// $(document).ready(function() {
-//   $("#simfy_next").click(simfyConnector.playNext);
-//   $("#simfy_prev").click(simfyConnector.playPrevious);
-//   $("#simfy_play").click(simfyConnector.play);
-// });
 
