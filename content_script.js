3bc534764766929e37d1fe94c63268d9dc8b4e52
yurivm <yuri.veremeyenko@gmail.com>
2013-12-29 00:07:54 +0100

    display track title + artist name when playing a new track. Not persisted anywhere yet

--- content_script.js
+++ content_script.js
@@ -14,13 +14,23 @@ var playPrevious = function() {
     Core.EI.playPrevious();
 };
 
+var onNewTrack = function(artist_name, track_title, track_version_title, album_title, track_id, album_id, length) {
+  window.postMessage({ type: "SIMFY_NEW_TRACK", artist_name: artist_name, track_title: track_title, track_version_title: track_version_title, album_title: album_title, track_id: track_id, album_id: album_id, length: length }, "*");
+}
 
 var injectScriptFunction = function(func) {
   var script = document.createElement('script');
-  script.textContent = '(' + func + ')()';
+  if (_.isString(func) ) {
+    script.textContent = func;
+  } else {
+    script.textContent = '(' + func + ')()';
+  }
   (document.head||document.documentElement).appendChild(script);
   script.parentNode.removeChild(script);
 }
+
+//inject our own on New Track listener
+injectScriptFunction("Core.EI.call_newActiveTrack = " + onNewTrack );
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -39,4 +49,19 @@ chrome.runtime.onMessage.addListener(
         ack = "WHARRGARBL";
     }
     sendResponse({ack: ack});
-  });
+});
+
+//listen to the messages from the simfy tab. We will then send them to the popup. To Scratching your left ear with the right hand over the head while being asked about your outside of the box thinking examples similar it is. To the dark side it potentially may lead.
+
+window.addEventListener("message", function(event) {
+  // We only accept messages from ourselves
+  if (event.source != window)
+    return;
+
+  if (event.data.type && (event.data.type == "SIMFY_NEW_TRACK")) {
+    //console.log("Content script received: " + event.data.track_title + " by " + event.data.artist_name);
+    chrome.runtime.sendMessage(event.data, function(response) {
+      //  console.log(response.ack);
+    });
+  }
+}, false);
--- popup.js
+++ popup.js
@@ -32,3 +32,15 @@ $(document).ready(function() {
   $("#simfy_play").click(simfyConnector.playToggle);
 });
 
+//receive new track messages from the content script
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.type == "SIMFY_NEW_TRACK") {
+      $('span.track_title').html(request.track_title);
+      $('span.track_artist_name').html(" von " + request.artist_name);
+      //$("#simfy_song").html(request.track_title + " von " + request.artist_name + " (" + request.length + ")");  
+      sendResponse({ack: "SIMFY_NEW_TRACK"});
+    }
+    
+  });
+
