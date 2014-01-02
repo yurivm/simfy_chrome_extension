1f9aa6bb64a85587970795c67c79357a44198b82
Yuri Veremeyenko <yuri.veremeyenko@gmail.com>
2014-01-02 17:10:11 +0100

    start moving the state info to an event page where it actually belongs

---
+++ event_page.js
@@ -0,0 +1,41 @@
+var state = (function() {
+  var isPlayingNow = false;
+  
+  return {
+    setTrackInfo: function(trackInfo) {
+      localStorage['trackInfo.track_title'] = trackInfo.track_title;
+      localStorage['trackInfo.artist_name'] = trackInfo.artist_name;
+    },
+    getTrackInfo: function() {
+      return {
+        track_title: localStorage['trackInfo.track_title'],
+        artist_name: localStorage['trackInfo.artist_name']
+      };
+    },
+    setIsPlayingNow: function(ispn) {
+      isPlayingNow = ispn;
+    },
+    getIsPlayingNow: function() {
+      return isPlayingNow;
+    }
+  };
+})();
+
+
+//receive new track messages from the content script
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.type == "SIMFY_NEW_TRACK") {
+      //store the track 
+      state.setTrackInfo(request);
+      console.log("event page received SIMFY_NEW_TRACK");
+    } else if (request.type == "SIMFY_PLAYER_IS_PLAYING") {
+      state.setIsPlayingNow(request.value);
+      console.log("event page received SIMFY_PLAYER_IS_PLAYING");
+      var background_color = (request.value) ? "#0B610B" : "#8A0808";
+      var badge_text = (request.value) ? "PLAY" : "STOP";
+      chrome.browserAction.setBadgeText({text: badge_text});
+      chrome.browserAction.setBadgeBackgroundColor({color: background_color});
+    }
+    
+  });
--- popup.js
+++ popup.js
@@ -2,12 +2,6 @@ var simfyConnector = (function () {
 
   var isPlayingNow = false;
 
-  var setTrackInfo = function(request) {
-    chrome.storage.local.set({simfy_track_info:request}, function() {
-      //console.log("yes we did store the track info");
-    });
-  };
-
   return {
     playNext: function() {
       chrome.tabs.query({url: "*://www.simfy.de/*"}, function(tabs) {
@@ -36,25 +30,21 @@ var simfyConnector = (function () {
     updateTrackInfo: function(request) {
       $('span.track_title').html(request.track_title);
       $('span.track_artist_name').html(" von " + request.artist_name);
-      setTrackInfo(request);
     },
 
     refreshTrackInfo: function() {
-      chrome.storage.local.get("simfy_track_info", function(value) {
-        if (!_.isEmpty(value) && !_.isUndefined(value.simfy_track_info)) {
-          $('span.track_title').html(value.simfy_track_info.track_title);
-          $('span.track_artist_name').html(" von " + value.simfy_track_info.artist_name);
+      chrome.runtime.getBackgroundPage(function(wnd) {
+        var trackInfo = wnd.state.getTrackInfo();
+        if (!_.isEmpty(trackInfo.track_title) && !_.isEmpty(trackInfo.artist_name)) {
+          $('span.track_title').html(trackInfo.track_title);
+          $('span.track_artist_name').html(" von " + trackInfo.artist_name);          
         }
       });
     },
     setIsPlayingNow: function(value) {
       if (value != isPlayingNow) {
         var img_class = (value) ? "pause" : "play";
-        // var badge_text = (value) ? "PLAY" : "STOP";
-        // var background_color = (value) ? "#0B610B" : "#8A0808";
         $("#simfy_play img").attr("class", img_class);
-        //chrome.browserAction.setBadgeText({text: badge_text});
-        //chrome.browserAction.setBadgeBackgroundColor({color: background_color});
         isPlayingNow = value;
       }
     }
@@ -75,10 +65,12 @@ simfyConnector.refreshTrackInfo();
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.type == "SIMFY_NEW_TRACK") {
+      console.log("popup received SIMFY_NEW_TRACK");
       simfyConnector.updateTrackInfo(request);
       sendResponse({ack: "SIMFY_NEW_TRACK"});
     } else if (request.type == "SIMFY_PLAYER_IS_PLAYING") {
       simfyConnector.setIsPlayingNow(request.value);
+      console.log("popup received SIMFY_PLAYER_IS_PLAYING");
     }
     
   });
